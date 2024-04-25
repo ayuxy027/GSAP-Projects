9609f883ed5b52335a0e8aea945b439699eff3aa
Ayush Yadav <ayush421301@gmail.com>
2024-04-26 02:08:44 +0530

    Added Enhancements

--- Splitting Boxes/script.js
+++ Splitting Boxes/script.js
@@ -1,12 +1,22 @@
 gsap.to("#box", {
     x:1000,
     duration: 2,
-    delay : 1
-})
+    delay : 1,
+    rotate:360
+}
+)
 
 gsap.to("#box2", {
     x:500,
-    y:250,
+    y:160,
     duration: 2,
-    delay : 1
+    delay : 1,
+    rotate:360
+})
+
+gsap.to("#text", {
+    x:700,
+    duration: 2,
+    delay : 1,
+    rotate:720
 })
