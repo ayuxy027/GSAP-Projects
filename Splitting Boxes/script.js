84991934f158eb98f9214afc35cebc712de47f10
Ayush Yadav <ayush421301@gmail.com>
2024-05-15 22:12:47 +0530

    Used "Stagger", "Timeline" and "Yoyo" Property

--- Splitting Boxes/script.js
+++ Splitting Boxes/script.js
@@ -1,22 +1,30 @@
 gsap.to("#box", {
     x:1000,
-    duration: 2,
+    duration: 4,
     delay : 1,
-    rotate:360
+    rotate:360,
+    backgroundColor:"teal",
+    repeat:-1,
+    yoyo:true,
 }
 )
 
 gsap.to("#box2", {
     x:500,
     y:160,
-    duration: 2,
-    delay : 1,
-    rotate:360
+    duration: 4,
+    rotate:360,
+    delay : 1.5,
+    borderRadius:"50%",
+    backgroundColor:"crimson",
+    scale:1.5,
+    repeat:-1,
+    yoyo:true,
 })
 
 gsap.to("#text", {
     x:700,
-    duration: 2,
+    duration: 4,
     delay : 1,
-    rotate:720
+    scale:1.75
 })
---
+++ Timeline/script.js
@@ -0,0 +1,26 @@
+// We can use the function of Timeline here 
+var tl =gsap.timeline();
+
+
+tl.to("#text1", {
+    x:300,
+    duration: 1,
+    delay: 1,
+    y:200,
+    stagger:0.3
+})
+
+tl.to("#text2", {
+    x:400,
+    duration: 0.7,
+    delay: 0.5,
+    y:250,
+    stagger:0.3
+})
+tl.to("#text3", {
+    x:500,
+    duration: 0.7,
+    delay: 0.5,
+    y:300,
+    stagger:0.3
+})
